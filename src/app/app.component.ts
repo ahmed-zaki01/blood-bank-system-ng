e5553ff2a8cef6835a25391dc9baf0d5cf28d5d9
Ahmed Zaki <ahmed.zaki0263@gmail.com>
2019-09-12 22:55:48 +0200

    configure font-awesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare let $:any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'blood-bank-system';
+
+  ngOnInit() {
+    $(document).ready(function() {
+      alert('I am Called From jQuery');
+    });
+  }
 }
